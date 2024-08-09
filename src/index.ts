9042e5b6c178f4f861e2fd2d070243301a0274a8
Dmitry Bikman <dmitry@bikman.ru>
2024-08-10 01:17:02 +0300

    Reorganized exports to allow import from root
    package.json version update to 3.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,197 +1,67 @@
-import {createHash, createHmac} from 'crypto';
-import {
-    type BadgeDto,
-    BadgeSchema,
-    type CategoriesGroupsTagsBadgesDto,
-    CategoriesGroupsTagsBadgesSchema,
-    type CategoryDto,
-    CategorySchema,
-    type GroupDto,
-    GroupSchema,
-    type TagDto,
-    TagSchema
-} from './dtos/fronts.js';
-import {
-    type ProductCardDto,
-    ProductCardSchema,
-    type ProductCardOnHomepageDto,
-    ProductCardOnHomepageSchema,
-    type ProductDto,
-    ProductSchema,
-    type ScheduleSlotDto,
-    ScheduleSlotSchema,
-    type ScheduleSlotTariffsDto,
-    ScheduleSlotTariffsSchema,
-} from './dtos/products.js';
-import {APIErrorSchema} from './dtos/errors.js';
-import APIError from './error.js';
+import DclApiClient from './dcl-api-client.js';
+
+export {DclApiClient};
+
+
 import UnixTimestamp from './unixtimestamp.js';
 
-const VERSION: string = '3';
+export {UnixTimestamp};
 
-export default class DclApi {
-    private readonly base_url: string;
-    private readonly front: string;
-    private readonly key: string;
-    private readonly secret: string;
 
-    constructor(base_url: string, front: string, key: string, secret: string) {
-        this.base_url = base_url;
-        this.front = front;
-        this.key = key;
-        this.secret = secret;
-    }
+import APIError from './error.js';
 
-    private signRequest(request: Request): string {
-        const u = new URL(request.url);
-        u.searchParams.sort();
-        const canonicalUri: string = u.pathname.trim();
-        const canonicalQueryString: string = u.searchParams.toString().trim();
-        const canonicalHeaders: string = [...request.headers.keys()]
-            .sort()
-            .reduce((acc, obj) => acc += `${obj.toLowerCase().trim()}:${request.headers.get(obj)?.toLowerCase().trim().replace(/\s\s+/g, ' ')}\n`, '')
-            .trim();
-        const signedHeaders: string = [...request.headers.keys()]
-            .sort()
-            .reduce((acc, obj) => acc += `${obj.toLowerCase().trim()};`, '').slice(0, -1)
-            .trim();
-        const payload: string = request.body ? JSON.stringify(request.body) : '';
-        const hashedPayload: string = createHash('sha256')
-            .update(new TextEncoder().encode(payload))
-            .digest('hex');
-        const canonicalRequest: string = `${request.method.trim()}\n${canonicalUri}\n${canonicalQueryString}\n${canonicalHeaders}\n${signedHeaders}\n${hashedPayload}`;
-        return createHmac('sha256', this.secret)
-            .update(new TextEncoder().encode(canonicalRequest))
-            .digest('hex');
-    }
+export {APIError};
 
-    private prepareRequest(method: 'GET' | 'HEAD' | 'POST' | 'PUT' | 'PATCH' | 'DELETE', url: string, body: BodyInit | null): Request {
-        const headersInit: HeadersInit = {
-            'Accept': 'application/json; charset=UTF-8',
-            'X-Front': this.front,
-            'X-API-Version': VERSION,
-            'X-API-Key': this.key,
-            'X-API-Timestamp': new Date().toISOString(),
-            'X-API-Signed-Headers': '',
-        };
 
-        const reqInit: RequestInit = {
-            method: method,
-            mode: 'cors',
-            redirect: 'follow',
-            headers: headersInit,
-            body: body,
-        };
+import {
+    type CategoryDto,
+    type GroupDto,
+    type TagDto,
+    type BadgeDto,
+    type CategoriesGroupsTagsBadgesDto,
+} from './dtos/fronts.js';
 
-        const req = new Request(url, reqInit);
+export {
+    type CategoryDto,
+    type GroupDto,
+    type TagDto,
+    type BadgeDto,
+    type CategoriesGroupsTagsBadgesDto,
+};
 
-        req.headers.set('X-API-Signed-Headers',
-            [...req.headers.keys()]
-                .sort()
-                .reduce((acc, obj) => acc += `${obj.toLowerCase().trim()};`, '')
-                .slice(0, -1)
-                .trim()
-        );
-        const signature = this.signRequest(req);
-        req.headers.set('X-API-Signature', signature);
 
-        return req;
-    }
+import {
+    ProductCardDto,
+    ProductCardOnHomepageDto,
+    ProductDto,
+    ScheduleSlotDto,
+    ScheduleSlotTariffsDto,
+} from './dtos/products.js';
 
-    private async get(url: string): Promise<any> {
-        const req = this.prepareRequest('GET', url, null);
-        const res = await fetch(req);
-        if (res.ok) {
-            return res.json();
-        } else {
-            const ae = await APIErrorSchema.parseAsync(await res.json());
-            throw new APIError(ae);
-        }
-    }
+export {
+    type ProductCardDto,
+    type ProductCardOnHomepageDto,
+    type ProductDto,
+    type ScheduleSlotDto,
+    type ScheduleSlotTariffsDto,
+};
 
-    private async create(fetch: any, url: string, body: any): Promise<any> {
-        const req = this.prepareRequest('POST', url, JSON.stringify(body));
-        const res = await fetch(req);
-        if (res.ok) {
-            return res.json();
-        } else {
-            const ae = await APIErrorSchema.parseAsync(await res.json());
-            throw new APIError(ae);
-        }
-    }
 
-    public async getCategories(): Promise<CategoryDto[]> {
-        const res = await this.get(`${this.base_url}/fronts/categories/`);
-        return CategorySchema.array().parseAsync(res);
-    }
+import {CardTypes} from './dtos/fronts.js';
 
-    public async getGroups(): Promise<GroupDto[]> {
-        const res = await this.get(`${this.base_url}/fronts/groups/`);
-        return GroupSchema.array().parseAsync(res);
-    }
+export {CardTypes};
 
-    public async getTags(): Promise<TagDto[]> {
-        const res = await this.get(`${this.base_url}/fronts/tags/`);
-        return TagSchema.array().parseAsync(res);
-    }
 
-    public async getBadges(): Promise<BadgeDto[]> {
-        const res = await this.get(`${this.base_url}/fronts/badges/`);
-        return BadgeSchema.array().parseAsync(res);
-    }
+import {
+    SaleSchemas,
+    ImageTypes,
+    AccessTypes,
+    ScheduleSlotTariffsStatuses
+} from './dtos/products.js';
 
-    public async getCategoriesGroupsTagsBadges(): Promise<CategoriesGroupsTagsBadgesDto> {
-        const res = await this.get(`${this.base_url}/fronts/categories-groups-tags-badges/`);
-        return CategoriesGroupsTagsBadgesSchema.parseAsync(res);
-    }
-
-    public async getProducts(): Promise<ProductDto[]> {
-        const res = await this.get(`${this.base_url}/products/`);
-        return ProductSchema.array().parseAsync(res);
-    }
-
-    public async getProduct(slug: string): Promise<ProductDto> {
-        const res = await this.get(`${this.base_url}/products/${slug}/`);
-        return ProductSchema.parseAsync(res);
-    }
-
-    public async getProductScheduleSlots(slug: string): Promise<ScheduleSlotDto[]> {
-        const res = await this.get(`${this.base_url}/products/schedule-slots/${slug}/`);
-        return ScheduleSlotSchema.array().parseAsync(res);
-    }
-
-    public async getProductScheduleSlotTariffs(slug: string, scheduleId: number, slotFrom: UnixTimestamp, slotUntil: UnixTimestamp): Promise<ScheduleSlotTariffsDto> {
-        const res = await this.get(`${this.base_url}/products/tariffs/${slug}/${scheduleId}/${slotFrom.toUnixTimestamp()}/${slotUntil.toUnixTimestamp()}/`);
-        return ScheduleSlotTariffsSchema.parseAsync(res);
-    }
-
-    public async getProductCardsOnHomepage(): Promise<ProductCardOnHomepageDto[]> {
-        const res = await this.get(`${this.base_url}/products/cards/`);
-        return ProductCardOnHomepageSchema.array().parseAsync(res);
-    }
-
-    public async getProductCardsInCategory(slug: string): Promise<ProductCardDto[]> {
-        const res = await this.get(`${this.base_url}/products/cards/categories/${slug}/`);
-        return ProductCardSchema.array().parseAsync(res);
-    }
-
-    public async getProductCardsInGroup(slug: string): Promise<ProductCardDto[]> {
-        const res = await this.get(`${this.base_url}/products/cards/groups/${slug}/`);
-        return ProductCardSchema.array().parseAsync(res);
-    }
-
-    public async getProductCardsWithTag(slug: string): Promise<ProductCardDto[]> {
-        const res = await this.get(`${this.base_url}/products/cards/tags/${slug}/`);
-        return ProductCardSchema.array().parseAsync(res);
-    }
-
-    public async getProductCardsWithBadge(slug: string): Promise<ProductCardDto[]> {
-        const res = await this.get(`${this.base_url}/products/cards/badges/${slug}/`);
-        return ProductCardSchema.array().parseAsync(res);
-    }
-
-    public async getProductCardsOnDate(timestamp: UnixTimestamp): Promise<ProductCardDto[]> {
-        const res = await this.get(`${this.base_url}/products/cards/on/${timestamp.toUnixTimestamp()}/`);
-        return ProductCardSchema.array().parseAsync(res);
-    }
-}
+export {
+    SaleSchemas,
+    ImageTypes,
+    AccessTypes,
+    ScheduleSlotTariffsStatuses,
+};
