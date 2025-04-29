98aa578f9515bf62ecfc8c438fdd8f71db3459f4
Dmitry Bikman <dmitry@bikman.ru>
2025-04-29 05:10:50 +0300

    Drop of canonicalRequest debug print

--- src/index.ts
+++ src/index.ts
@@ -78,8 +78,6 @@ export default class DclApiClient {
 
         const canonicalRequest: string = `${request.method.trim()}\n${canonicalUri}\n${canonicalQueryString}\n${canonicalHeaders}\n${signedHeaders}\n${hashedPayload}`;
 
-        console.log(canonicalRequest);
-
         return createHmac('sha256', this.secret)
             .update(new TextEncoder().encode(canonicalRequest))
             .digest('hex');
